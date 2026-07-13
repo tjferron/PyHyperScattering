Fix I1 normalization: multiply corr by i1_interp instead of dividing
Corrects an error in the denominator of the reduced data for the 'als'
correction mode.

src/PyHyperScattering/ALS11012RSoXSLoader.py
@@ -284,7 +284,7 @@ class ALS11012RSoXSLoader(FileLoader):
                 i1_interp = (
                     np.interp(en, en1, pd1) / np.interp(en, en1, ai1)
                 ) / i0_interp
-                corr /= i1_interp
+                corr *= i1_interp
         else:
             corr = 1
 